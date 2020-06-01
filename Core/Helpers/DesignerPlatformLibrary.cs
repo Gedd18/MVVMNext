24c35384ec4ab96fb68f07e61ca2d1acfde88731
Gedd18 <github.gedd18@outlook.com>
2020-06-01 20:39:19 +0200

    MVVM Next project v1.0.0
    - compilation ok
    - Net 4.8
    - Net Core 3.1
    - Net 5.0 preview 4
    - some TODO
    - nuget packages

---
+++ Core/Helpers/DesignerPlatformLibrary.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace MvvmNext.Helpers
+{
+    /// <summary>
+    /// Helper class for platform detection.
+    /// </summary>
+    internal static class DesignerLibrary
+    {
+        internal static DesignerPlatformLibrary DetectedDesignerLibrary
+        {
+            get
+            {
+                if(_detectedDesignerPlatformLibrary == null)
+                {
+                    _detectedDesignerPlatformLibrary = GetCurrentPlatform();
+                }
+                return _detectedDesignerPlatformLibrary.Value;
+            }
+        }
+
+        private static DesignerPlatformLibrary GetCurrentPlatform()
+        {
+            // Check .NET 
+            var cmdm = Type.GetType("System.ComponentModel.DesignerProperties, PresentationFramework, Version=3.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
+            if (cmdm != null) // loaded the assembly, could be .net 
+            {
+                return DesignerPlatformLibrary.Net;
+            }
+
+            return DesignerPlatformLibrary.Other;
+        }
+
+
+        private static DesignerPlatformLibrary? _detectedDesignerPlatformLibrary;
+
+        private static bool? _isInDesignMode;
+
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                if (!_isInDesignMode.HasValue)
+                {
+                    var prop = System.ComponentModel.DesignerProperties.IsInDesignModeProperty;
+                    _isInDesignMode
+                        = (bool)System.ComponentModel.DependencyPropertyDescriptor
+                                        .FromProperty(prop, typeof(System.Windows.FrameworkElement))
+                                        .Metadata.DefaultValue;
+                }
+
+                return _isInDesignMode.Value;
+            }
+        }
+    }
+
+    internal enum DesignerPlatformLibrary
+    {
+        Unknown,
+        Net,
+        Other
+    }
+}
---
+++ makeAll.ps1
@@ -0,0 +1,34 @@
+
+$console = $host.ui.rawui
+
+$oldColor = $console.foregroundcolor
+$console.foregroundcolor = "green"
+"Build MvvmNext[Net48]"
+$console.foregroundcolor = $oldColor
+dotnet build MvvmNext[Net48].sln --verbosity quiet
+
+$console.foregroundcolor = "green"
+"Build MvvmNext[NetCore31]"
+$console.foregroundcolor = $oldColor
+dotnet build MvvmNext[NetCore31].sln --verbosity quiet
+
+$console.foregroundcolor = "green"
+"Build MvvmNext[Net50preview4]"
+$console.foregroundcolor = $oldColor
+dotnet build MvvmNext[Net50preview4].sln --verbosity quiet
+
+$console.foregroundcolor = "green"
+"Create package"
+"MVVMNext for Net48"
+$console.foregroundcolor = $oldColor
+dotnet pack MvvmNext[Net48].sln --output ./packages/Net48 --verbosity quiet
+
+$console.foregroundcolor = "green"
+"MVVMNext for NetCore31"
+$console.foregroundcolor = $oldColor
+dotnet pack MvvmNext[NetCore31].sln --output ./packages/NetCore31 --verbosity quiet
+
+$console.foregroundcolor = "green"
+"MVVMNext for Net50preview4"
+$console.foregroundcolor = $oldColor
+dotnet pack MvvmNext[Net50preview4].sln --output ./packages/Net50 --verbosity quiet
